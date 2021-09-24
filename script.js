336349b9f00c018a70c39decaa5972281fdc8ef2
Chris Rebuck <crebuck.apt@gmail.com>
2021-09-24 09:00:00 -0500

    Adding hold button functionality and logic if player wins game.

--- script.js
+++ script.js
@@ -21,28 +21,64 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+//create state variable to determine if the game is being played or not
+//if playing the buttons are clickable
+//if not then buttons aren't clickable
+let playing = true;
+
+const switchPlayer = function () {
+  //Switch to next player and reset current score to 0
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 //Rolling Dice functionality
 btnRoll.addEventListener('click', function () {
-  //1.Generate a random number (dice roll) 1-6
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  if (playing) {
+    //1.Generate a random number (dice roll) 1-6
+    const dice = Math.trunc(Math.random() * 6) + 1;
 
-  //2. Display the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
+    //2. Display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
 
-  //3. Check for rolled 1;
-  if (dice != 1) {
-    //Add dice to the current score
-    currentScore += dice;
-    //Selecting the score element dynamically based on which player is active right now
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-  } else {
-    //Switch to next player and reset current score to 0
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    currentScore = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    //3. Check for rolled 1;
+    if (dice != 1) {
+      //Add dice to the current score
+      currentScore += dice;
+      //Selecting the score element dynamically based on which player is active right now
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    //add current score to the total score of active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //check if score is >=100
+    if (scores[activePlayer] >= 10) {
+      //if score >=100 current player wins the game
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      diceEl.classList.add('hidden');
+    } else {
+      //else switch players
+      switchPlayer();
+    }
   }
 });
