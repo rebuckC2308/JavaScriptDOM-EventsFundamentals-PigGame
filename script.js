c810ddee5b0852216df63ddae0481d3cfcd56914
Chris Rebuck <crebuck.apt@gmail.com>
2021-09-24 08:35:21 -0500

    Add functionality to switch players based on dice roll.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const currentScore0El = document.getElementById('current--0');
 const currentScore1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -15,7 +17,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling Dice functionality
 btnRoll.addEventListener('click', function () {
@@ -30,8 +34,15 @@ btnRoll.addEventListener('click', function () {
   if (dice != 1) {
     //Add dice to the current score
     currentScore += dice;
-    currentScore0El.textContent = currentScore; //Change Later for both players - who is active!
+    //Selecting the score element dynamically based on which player is active right now
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    //Switch to next player
+    //Switch to next player and reset current score to 0
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
